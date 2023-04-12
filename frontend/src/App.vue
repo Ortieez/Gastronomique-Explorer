7c0c8161be295892966d7ccf5343e92aa9790019
Simon Kašpar <simonkaspar9@seznam.cz>
2023-04-12 09:32:03 +0200

    init setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+export default {
+ name: "Map",
+ data() {
+   return{
+     center: [46.21, 16.62]
+   }},
+ methods: {
+   setupLeafletMap: function () {
+     const mapDiv = L.map("mapContainer").setView(this.center, 13);
+     L.tileLayer(
+       "https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5j2fAuonON9Q6uInrMgp",
+       {
+        crossOrigin: true
+         
+       }
+     ).addTo(mapDiv);
+   },
+ },
+ mounted() {
+   this.setupLeafletMap();
+ },
+};
+</script>
+
+<template>
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+
+      <div id="container">
+        <div id="mapContainer"></div>
+      </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+#mapContainer {
+ width: 80vw;
+ height: 100vh;
+}
+</style>
