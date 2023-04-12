8980f924256f1869bed86c9e25ff74458a661bfe
Simon Kašpar <simonkaspar9@seznam.cz>
2023-04-12 12:49:12 +0200

    First Marker

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,50 +1,53 @@
 <script>
-import "leaflet/dist/leaflet.css";
-import L from "leaflet";
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
 export default {
- name: "Map",
- data() {
-   return{
-     center: [46.21, 16.62]
-   }},
- methods: {
-   setupLeafletMap: function () {
-     const mapDiv = L.map("mapContainer").setView(this.center, 13);
-     L.tileLayer(
-       "https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5j2fAuonON9Q6uInrMgp",
-       {
-        crossOrigin: true
-         
-       }
-     ).addTo(mapDiv);
-   },
- },
- mounted() {
-   this.setupLeafletMap();
- },
-};
+  name: 'Map',
+  data() {
+    return {
+      center: [50.0755, 14.4378]
+    }
+  },
+  methods: {
+    setupLeafletMap: function (mapDiv) {
+      L.tileLayer(
+        'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=5j2fAuonON9Q6uInrMgp',
+        {
+          crossOrigin: true
+        }
+      ).addTo(mapDiv)
+    },
+    addMarker: function (mapDiv) {
+      L.marker([50.479217, 14.0803621]).addTo(mapDiv)
+      fetch('/api/places')
+        .then((response) => response.json())
+        .then((text) => {
+          text.forEach(place => {
+            
+          });
+        })
+    }
+  },
+  mounted() {
+    const mapDiv = L.map('mapContainer').setView(this.center, 8)
 
-fetch('/api/places')
-.then(response => response.text())
-.then(text => console.log(text))
+    this.setupLeafletMap(mapDiv)
+    this.addMarker(mapDiv)
+  }
+}
 </script>
 
 <template>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-
-      <div id="container">
-        <div id="mapContainer"></div>
-      </div>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
 
   <RouterView />
 </template>
 
 <style scoped>
 #mapContainer {
- width: 80vw;
- height: 100vh;
+  width: 80vw;
+  height: 100vh;
 }
 </style>
