4d294e81c905fc26caac29fd7428c1fa6a730c61
Simon Kašpar <simonkaspar9@seznam.cz>
2023-04-19 21:56:22 +0200

    About page

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,3 +1,86 @@
-<template> 
+<script>
+</script>
+
+<template>
+    
+    <h1 id="title"> About this project </h1>
+    <div class="maintext">
+        <h2> What's the purpose of Gastronomique Explorer </h2>
+        <p> The main purpose of the Gastronomique Explorer is to provide remote drivers or drivers in general with a map of locations where they can stop for refueling, food and drink, including carrying out necessities on toilet. </p>
+    </div>
+    <p> We are currently working on a marker system where you can add a marker anywhere you like on the map and then save it for next time, and also on bars for a better overview of a given place. </p>
+    <p> If you want to know more about this project you can visit our github a read more. </p>
+
+    <div class="secondtext">
+        <h2 id="secondtitle"> Why was this project created </h2>
+        <p> It was created as a school project to test our skills. We are a two-person team and we split the work - frontend and backend.
+            <br> If you like to know more about us visit our personal githubs.
+        </p>
+    </div>
+
+    <div class="github">
+        <a href="https://github.com/Ortieez/Gastronomique-Explorer">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+        </a>
+        <a href="https://github.com/Gh0st-ed"> 
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+        </a>
+        <a href="https://github.com/Ortieez">
+            <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 24"><path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-4.466 19.59c-.405.078-.534-.171-.534-.384v-2.195c0-.747-.262-1.233-.55-1.481 1.782-.198 3.654-.875 3.654-3.947 0-.874-.312-1.588-.823-2.147.082-.202.356-1.016-.079-2.117 0 0-.671-.215-2.198.82-.64-.18-1.324-.267-2.004-.271-.68.003-1.364.091-2.003.269-1.528-1.035-2.2-.82-2.2-.82-.434 1.102-.16 1.915-.077 2.118-.512.56-.824 1.273-.824 2.147 0 3.064 1.867 3.751 3.645 3.954-.229.2-.436.552-.508 1.07-.457.204-1.614.557-2.328-.666 0 0-.423-.768-1.227-.825 0 0-.78-.01-.055.487 0 0 .525.246.889 1.17 0 0 .463 1.428 2.688.944v1.489c0 .211-.129.459-.528.385-3.18-1.057-5.472-4.056-5.472-7.59 0-4.419 3.582-8 8-8s8 3.581 8 8c0 3.533-2.289 6.531-5.466 7.59z"/></svg>
+        </a>
+    </div>
+
+    <div class="names">
+        <p> Gastronomique-Explorer </p>
+        <p> Ghost-dev </p>
+        <p> Ortieez-dev </p>
+    </div>
 
 </template>
+
+<style> 
+
+.tempate {
+    background-image: url(background.png);
+}
+#title {
+    text-align: center;
+    font-family: Consolas, 'Courier New', Courier, monospace;
+    font-size: xxx-large;
+}
+
+.maintext h2 {
+    text-align: center;
+    font-family: Consolas, 'Courier New', Courier, monospace;
+}
+.maintext p {
+    text-align: center;
+    font-family: Consolas, 'Courier New', Courier, monospace;
+}
+
+#secondtitle {
+    font-size: xxx-large;
+}
+
+.secondtext h2, p {
+    text-align: center;
+    font-family: Consolas,'Courier New', Courier, monospace;
+}
+
+.github {
+    display: flex;
+    justify-content: center;
+    gap: 100px;
+}  
+
+.github p {
+    display: flex;
+}
+
+.names {
+    display: flex;
+    justify-content: center;
+    gap: 100px;
+}
+
+</style>
